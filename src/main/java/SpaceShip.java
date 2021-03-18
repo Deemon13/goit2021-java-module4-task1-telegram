3fbb627403aad130c97119dad53ed34dac28a018
Dmitriy Fedorenko <dmitriy.fedorenko13@gmail.com>
2021-03-18 22:56:49 +0200

    added methods get&set to class SpaceShip

--- src/main/java/SpaceShip.java
+++ src/main/java/SpaceShip.java
@@ -1,3 +1,19 @@
-class SpaceShip {
-    public String name = "Voyager";
+public class SpaceShip {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    //Test output
+    public static void main(String[] args) {
+        SpaceShip ship = new SpaceShip();
+        System.out.println(ship.getName()); //Should be null
+        ship.setName("Walker");
+        System.out.println(ship.getName()); //Should be Walker
+    }
 }

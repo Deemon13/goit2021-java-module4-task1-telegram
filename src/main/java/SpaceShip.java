516d614170340eaa323fb1eac0f56a5c95035cd3
Dmitriy Fedorenko <dmitriy.fedorenko13@gmail.com>
2021-03-19 20:15:17 +0200

    added methods setSerialNumber & getSerialNumber of class SpaceShip

--- src/main/java/SpaceShip.java
+++ src/main/java/SpaceShip.java
@@ -1,14 +1,26 @@
 public class SpaceShip {
     private String name;
+    private String serialNumber;
 
     public String getName() {
         return name;
+    }
+
+    public String getSerialNumber() {
+        return serialNumber;
     }
 
     public void setName(String name) {
         if (!name.isBlank() && name.length() <= 100) {
             this.name = name;
         }
+    }
+
+    public void setSerialNumber(String serialNumber) {
+        if (serialNumber.contains("SN") && serialNumber.length() == 8) {
+            this.serialNumber = serialNumber;
+        }
+        return;
     }
 
     //Test output
@@ -24,5 +36,10 @@ public class SpaceShip {
         System.out.println(ship.getName()); //Should be Walker, empty value ignored
         ship.setName("Voyager ".repeat(100));
         System.out.println(ship.getName()); //Should be Walker, too long value
+
+        ship.setSerialNumber("SN506788");
+        System.out.println(ship.getSerialNumber()); //Should be SN506788
+        ship.setSerialNumber("EE123456");
+        System.out.println(ship.getSerialNumber()); //Should be SN506788 - old value
     }
 }

37e750402dc2df054b306ded2116e3318d43be26
Dmitriy Fedorenko <dmitriy.fedorenko13@gmail.com>
2021-03-20 18:48:19 +0200

    added class SpaceshipCalc

---
+++ src/main/java/SpaceshipCalc.java
@@ -0,0 +1,15 @@
+class SpaceshipCalc {
+    public final int calculateAirBalloons(int passengerCount) {
+        return 10 * passengerCount;
+    }
+
+    public int calculateBookCount(int passengerCount) {
+        return 2 * passengerCount;
+    }
+
+    public static void main(String[] args) {
+        SpaceshipCalc calc = new SpaceshipCalc();
+        System.out.println(calc.calculateAirBalloons(35)); // 350
+        System.out.println(calc.calculateBookCount(50)); // 100
+    }
+}

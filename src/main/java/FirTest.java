484d5dec1dd480648b77b089b37b77c7511bebde
Dmitriy Fedorenko <dmitriy.fedorenko13@gmail.com>
2021-03-21 18:03:57 +0200

    added class FirNumSum

--- src/main/java/FirTest.java
+++ src/main/java/FirTest.java
@@ -7,13 +7,30 @@ class FirTest {
         FirNum firNum = new FirNum();
         FirTest firTest = new FirTest();
 
+
+
         //Should be 10
         System.out.println(firTest.test(firNum, 10));
+
+        //Should be 6
+        System.out.println(new FirNumSum().calc(3));
     }
 }
 
 class FirNum {
     public int calc(int n) {
         return n;
+    }
+}
+
+class FirNumSum extends FirNum {
+
+    @Override
+    public int calc(int n) {
+        int numSum = 0;
+        for (int i = 0; i <= n; i++) {
+            numSum += i;
+        }
+        return numSum;
     }
 }

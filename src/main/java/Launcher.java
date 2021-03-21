44fa66f3c8932553bcb3c747d3a251097f754897
Dmitriy Fedorenko <dmitriy.fedorenko13@gmail.com>
2021-03-21 22:04:14 +0200

    added class CompressorStation

--- src/main/java/Launcher.java
+++ src/main/java/Launcher.java
@@ -7,14 +7,30 @@ class Launcher {
 
 class Spaceport {
     private GasStation gasStation = new GasStation();
+    private ServiceTower serviceTower = new ServiceTower();
+    private CompressorStation compressorStation = new CompressorStation();
 
     public void launch() {
         gasStation.refuel();
+        serviceTower.service();
+        compressorStation.compress();
     };
 }
 
 class GasStation {
     public void refuel() {
         System.out.println("Refuel done!");
+    }
+}
+
+class ServiceTower {
+    public void service() {
+        System.out.println("Service done!");
+    }
+}
+
+class CompressorStation {
+    public void compress() {
+        System.out.println("Compress done!");
     }
 }
